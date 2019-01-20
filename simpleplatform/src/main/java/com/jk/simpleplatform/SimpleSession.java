1bbfcbc7a56e9a71c8300aa8f338993bd9f613b8
RohJK <rohjk93@kpu.ac.kr>
2019-01-20 15:27:12 +0900

    Add SimpleAuthProvider ChangeName SimpleAuth to SimpleSession

---
+++ simpleplatform/src/main/java/com/jk/simpleplatform/SimpleSession.java
@@ -0,0 +1,74 @@
+package com.jk.simpleplatform;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.jk.simpleplatform.idp.AuthClient;
+import com.jk.simpleplatform.idp.IdpType;
+
+public class SimpleSession {
+    private static String TAG = "[SimpleSession]";
+    private Activity activity;
+
+    private static AuthClient currentClient;
+
+    public SimpleSession(){
+        currentClient = null;
+    }
+
+    public static void setAuthProvider(@NonNull IdpType idpType, @NonNull String serverId){
+        if(idpType!=null && serverId !=null )
+            SimpleAuthprovider.getInstance().addServerId(idpType,serverId);
+    }
+
+    public static void login(@NonNull Activity activity, @NonNull IdpType idpType, @NonNull final SimpleAuthResultCallback<Void> callback){
+        if(activity == null){
+            callback.onResult(SimpleAuthResult.<Void>getFailResult(-100,"Activity is Null"));
+
+        }else if(idpType == null){
+            callback.onResult(SimpleAuthResult.<Void>getFailResult(-101,"Idp Type is Null"));
+
+        }else if (callback == null){
+            Log.d(TAG,"Login Callback is null");
+
+        }else{
+            final AuthClient authClient = AuthClient.getAuthClientInstance(idpType);
+
+            SimpleSession.currentClient = authClient;
+
+            authClient.login(activity, new SimpleAuthResultCallback<Void>() {
+                @Override
+                public void onResult(SimpleAuthResult<Void> result) {
+
+                    if(!result.isSuccess()){
+                        SimpleSession.currentClient = null;
+                    }
+
+                   callback.onResult(result);
+                }
+            });
+        }
+    }
+
+    public static void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(currentClient!=null){
+            currentClient.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+
+    public static IdpType getCurrentIdpType()
+    {
+        if(currentClient!=null)
+            return currentClient.getIdpType();
+        return null;
+    }
+
+    public static String getAccessToken(){
+        if(currentClient!=null)
+            return currentClient.getToken();
+        return null;
+    }
+
+}
